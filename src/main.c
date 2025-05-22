fff14c8b9c486fc2d6ebf3c0a2a8a240013b6969
João Pinheiro <guifranco01@gmail.com>
2025-05-21 21:09:27 -0300

    testando

--- src/main.c
+++ src/main.c
@@ -1,1 +1,15 @@
-testando
+#include <stdio.h>    // entrada e saída
+#include <stdlib.h>   // malloc, free
+#include <string.h>   // manipulação de strings
+#include <limits.h>   // INT_MAX para infinito
+#include <stdbool.h>  // para usar true/false
+
+
+
+int main(){
+
+printf("ola");
+
+return 0;
+
+}
