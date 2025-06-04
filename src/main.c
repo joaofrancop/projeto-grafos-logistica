b8b480e0b5957f2901e9d49b12ad214138dcf51e
João Guilherme Franco Pinheiro <guifranco01@gmail.com>
2025-06-04 20:43:07 -0300

    Corrige acentuação no terminal com UTF-8 e configura ambiente VS Code

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include "interface.h"
+#include <stdio.h>
+
+#include <windows.h>
 
 int main() {
+
+    SetConsoleOutputCP(65001);  // saída (printf)
+    SetConsoleCP(65001);        // entrada (scanf)
+
     exibirMenu();
     return 0;
 }
