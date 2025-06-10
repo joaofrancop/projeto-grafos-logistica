8e9cc4a45afd7995f597caed888404672645d847
João Guilherme Franco Pinheiro <116494379+joaofrancop@users.noreply.github.com>
2025-06-10 19:59:13 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,6 +4,10 @@
 #include <windows.h>
 
 int main() {
+    // Configura console para UTF-8
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
     // Cria o grafo com 6 vértices (0 a 5)
     Grafo* g = criarGrafo(6);
 
@@ -16,23 +20,11 @@ int main() {
     adicionarAresta(g, 4, 3, 2);
     adicionarAresta(g, 3, 5, 1);
 
-    // Imprime a estrutura do grafo
-    printf("Grafo:\n");
-    imprimirGrafo(g);
-
-    // Executa busca A* do vértice 0 até o 5
-    printf("\nBusca A* de V0 até V5:\n");
-    a_estrela(g, 0, 5);
+    // Inicia o menu interativo, passando o grafo
+    exibirMenu(g);
 
     // Libera memória
     liberarGrafo(g);
-    
-
-    SetConsoleOutputCP(65001);  // saída (printf)
-    SetConsoleCP(65001);        // entrada (scanf)
-
-    exibirMenu();
-
 
     return 0;
 }
